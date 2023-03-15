545f98d85ea204aa9b79edb3dffdd4c0d6e1ddb6
webozza <webozza@gmail.com>
2023-03-15 19:10:44 +0800

    version update

--- evaluation-calc.php
+++ evaluation-calc.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Evaluation Calculator
  * Plugin URI:        https://webozza.com
  * Description:       Custom plugin developed by @webozza
- * Version:           4.0.0
+ * Version:           5.0.0
  * Author:            Webozza
  * Author URI:        https://webozza.com
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'EVALUATION_CALC_VERSION', '4.0.0' );
+define( 'EVALUATION_CALC_VERSION', '5.0.0' );
 
 /**
  * The code that runs during plugin activation.
