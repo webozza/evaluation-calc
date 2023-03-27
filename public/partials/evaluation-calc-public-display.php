4a3a740c2177f8e534917c38d80ce56a9201c826
webozza <webozza@gmail.com>
2023-03-27 23:01:18 +0800

    version update

--- public/partials/evaluation-calc-public-display.php
+++ public/partials/evaluation-calc-public-display.php
@@ -882,7 +882,23 @@
 <?php } ?>
 
 <!-- GENERATE THE PDF FOR MAIL -->
-<?php echo plugin_dir_path(); ?>
+<?php 
+    require plugin_dir_path( __FILE__ ) . "public/fpdf/pdf.php" 
+    // Instantiate and use the FPDF class 
+    $pdf = new FPDF();
+    
+    //Add a new page
+    $pdf->AddPage();
+    
+    // Set the font for the text
+    $pdf->SetFont('Arial', 'B', 18);
+    
+    // Prints a cell with given text 
+    $pdf->Cell(60,20,'Hello GeeksforGeeks!');
+    
+    // return the generated output
+    $pdf->Output();
+?>
 
 <!-- EMAIL FUNC -->
 <?php if ( isset($_POST['time_estimate']) == 1 ) {
