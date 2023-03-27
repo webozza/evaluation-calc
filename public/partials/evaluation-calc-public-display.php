b40b59cc5ba8c9a6fc7e28e14039558390af8afb
webozza <webozza@gmail.com>
2023-03-27 23:07:42 +0800

    version update

--- public/partials/evaluation-calc-public-display.php
+++ public/partials/evaluation-calc-public-display.php
@@ -889,17 +889,7 @@
     // Instantiate and use the FPDF class 
     $pdf = new FPDF();
     
-    //Add a new page
-    $pdf->AddPage();
-    
-    // Set the font for the text
-    $pdf->SetFont('Arial', 'B', 18);
-    
-    // Prints a cell with given text 
-    $pdf->Cell(60,20,'Hello GeeksforGeeks!');
-    
-    // return the generated output
-    $pdf->Output();
+    var_dump($pdf);
 ?>
 
 <!-- EMAIL FUNC -->
