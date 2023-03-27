79fdbad1c21bf29a09b43112eeaba4f7d15f0f5c
webozza <webozza@gmail.com>
2023-03-27 22:04:04 +0800

    version update

--- public/partials/evaluation-calc-public-display.php
+++ public/partials/evaluation-calc-public-display.php
@@ -21,8 +21,8 @@
     <style>h1.wp-block-post-title {display:none;}</style>
     <div class="initial--content">
         <img src="<?= $plugin_dir . '/public/img/logo.jpeg' ?>">
-        <h4>Monitoring and Evaluation Plan</h4>
-        <h5>Capacity calculator</h5>
+        <h5>Monitoring and Evaluation Plan</h5>
+        <h4>Capacity calculator</h4>
         <p>This calculator estimates the number of hours that may be required to design your evaluation and/or monitoring plan. The result is only an estimate, but your responses to these 9 questions are vital to form an initial view of what may be required.</p>
     </div>
     <div class="evaluation-form-container">
