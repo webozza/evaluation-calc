f1d6450f40365047417295e9b9cc7b96db884a41
webozza <webozza@gmail.com>
2023-03-29 22:30:03 +0800

    version update

--- public/partials/evaluation-calc-public-display.php
+++ public/partials/evaluation-calc-public-display.php
@@ -895,53 +895,16 @@
         $metaDatas = stream_get_meta_data($tempfile);
         $tmpFilename = $metaDatas['uri'];
 
-        class PDF extends FPDF {
-
-            const DPI = 96;
-            const MM_IN_INCH = 25.4;
-            const A4_HEIGHT = 297;
-            const A4_WIDTH = 210;
-            // tweak these values (in pixels)
-            const MAX_WIDTH = 800;
-            const MAX_HEIGHT = 500;
-        
-            function pixelsToMM($val) {
-                return $val * self::MM_IN_INCH / self::DPI;
-            }
-        
-            function resizeToFit($imgFilename) {
-                list($width, $height) = getimagesize($imgFilename);
-        
-                $widthScale = self::MAX_WIDTH / $width;
-                $heightScale = self::MAX_HEIGHT / $height;
-        
-                $scale = min($widthScale, $heightScale);
-        
-                return array(
-                    round($this->pixelsToMM($scale * $width)),
-                    round($this->pixelsToMM($scale * $height))
-                );
-            }
-        
-            function centreImage($img) {
-                list($width, $height) = $this->resizeToFit($img);
-        
-                // you will probably want to swap the width/height
-                // around depending on the page's orientation
-                $this->Image(
-                    $img, (self::A4_HEIGHT - $width) / 2,
-                    (self::A4_WIDTH - $height) / 2,
-                    $width,
-                    $height
-                );
-            }
-        }
-
         $pdf = new FPDF();
+
+        $pdf->setPrintHeader(false);
+        $pdf->SetPrintFooter(false);
+        $pdf->SetMargins(0, 0, 0);
+        $pdf->SetAutoPageBreak(false, 0);
+
         $pdf->AddFont('Helvetica','','helvetica.php');
         $pdf->AddPage('L');
-        //$pdf->Image($tmpFilename,null,null,0,0,'PNG');
-        $pdf->centreImage($tmpFilename,null,null,0,0,'PNG');
+        $pdf->Image($tmpFilename, 220, 10, 200, 'PNG');
         $pdf->SetFont('helvetica','',16);
 
         $separator = md5(time());
