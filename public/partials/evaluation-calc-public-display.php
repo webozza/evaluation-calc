b9540008d53647e790c460d18d4bafa1498d0a6b
webozza <webozza@gmail.com>
2023-03-29 19:00:33 +0800

    version update

--- public/partials/evaluation-calc-public-display.php
+++ public/partials/evaluation-calc-public-display.php
@@ -18,6 +18,7 @@
 <!-- GLOBAL VARS -->
 <?php 
     $plugin_dir = '/wp-content/plugins/evaluation-calc';
+    $testhtml = "";
 ?>
 
 <!-- THE FORM -->
@@ -330,13 +331,14 @@
     <?php $plugin_dir = '/wp-content/plugins/evaluation-calc'; ?>
     <style>h1.wp-block-post-title {display:none;}</style>
     <?php
-    $testhtml = '
+    $testhtml2 = '
         <div class="initial--content">
             <img src="' .$plugin_dir. ' /public/img/logo.jpeg">
             <h4>Monitoring and Evaluation Plan</h4>
             <h4>Capacity calculator</h4>
         </div>
     ';
+    $testhtml = $testhtml2;
     ?>
 
     <!-- THE MATH HAPPENS OVER HERE -->
@@ -874,68 +876,67 @@
             <a href="javascript:void()" onclick="window.print()">Print my results</a>
         </div>
     </div>
-
-    <!-- EMAIL FUNC -->
-    <?php if ( isset($_POST['time_estimate']) == 1 ) {
-
-        add_filter( 'wp_mail_from', 'sender_email' );
-        function sender_email( $original_email_address ) {
-            return 'team@standardofproof.nz';
-        };
-
-        add_filter( 'wp_mail_from_name', 'sender_name' );
-        function sender_name( $original_email_from ) {
-            return 'Standard of Proof';
-        };
-
-        // //user posted variables
-        // $name = $_POST['client_name'];
-        // $email = $_POST['client_email'];
-        // $message = $name . ' just made a calculation.';
-
-        // //php mailer variables
-        // $to = 'webozza@gmail.com';
-        // $subject = $name . ' just made a calculation.';
-        // $headers = 'From: '. $email . "\r\n" .
-        //     'Reply-To: ' . $email . "\r\n";
-
-        // // Here put your Validation and send mail
-        // $sent = wp_mail($to, $subject, strip_tags($message), $headers);
-            
-        $email = 'webozza@gmail.com';
-        $subject = 'Your Evaluation is Ready';
-        $body = 'Please find your attached evaluation.';
-
-        // $pdf = new FPDF('P', 'pt', array(500,233));
-        $pdf = new PDF_HTML();
-        $pdf->AliasNbPages();
-
-        $pdf->SetAutoPageBreak(true, 15);
-
-        $pdf->AddFont('Helvetica','','helvetica.php');
-        $pdf->AddPage();
-        // $pdf->Image($plugin_dir . '/public/img/logo.jpeg',0,0,500);
-        $pdf->SetFont('helvetica','',16);
-        // $pdf->Cell(40,10,'Hello World!');
-        $pdf->WriteHTML($testhtml);
-
-        $separator = md5(time());
-
-        $headers = "MIME-Version: 1.0"; 
-        $headers .= "Content-Type: multipart/mixed; boundary=\"".$separator."\"";
-        $headers .= "Content-Transfer-Encoding: 7bit";
-
-        $pdf->Output($filename, "F"); // "F" is for saving the file on the server
-
-        $attachment = array($filename);
-
-        wp_mail( $email, $subject, $body, $headers, $attachment );
-
-        unlink($filename); // Deletion of the created file.
-        }
-    ?>
-
 <?php } ?>
+
+<!-- EMAIL FUNC -->
+<?php if ( isset($_POST['time_estimate']) == 1 ) {
+
+    add_filter( 'wp_mail_from', 'sender_email' );
+    function sender_email( $original_email_address ) {
+        return 'team@standardofproof.nz';
+    };
+
+    add_filter( 'wp_mail_from_name', 'sender_name' );
+    function sender_name( $original_email_from ) {
+        return 'Standard of Proof';
+    };
+
+    // //user posted variables
+    // $name = $_POST['client_name'];
+    // $email = $_POST['client_email'];
+    // $message = $name . ' just made a calculation.';
+
+    // //php mailer variables
+    // $to = 'webozza@gmail.com';
+    // $subject = $name . ' just made a calculation.';
+    // $headers = 'From: '. $email . "\r\n" .
+    //     'Reply-To: ' . $email . "\r\n";
+
+    // // Here put your Validation and send mail
+    // $sent = wp_mail($to, $subject, strip_tags($message), $headers);
+        
+    $email = 'webozza@gmail.com';
+    $subject = 'Your Evaluation is Ready';
+    $body = 'Please find your attached evaluation.';
+
+    // $pdf = new FPDF('P', 'pt', array(500,233));
+    $pdf = new PDF_HTML();
+    $pdf->AliasNbPages();
+
+    $pdf->SetAutoPageBreak(true, 15);
+
+    $pdf->AddFont('Helvetica','','helvetica.php');
+    $pdf->AddPage();
+    // $pdf->Image($plugin_dir . '/public/img/logo.jpeg',0,0,500);
+    $pdf->SetFont('helvetica','',16);
+    // $pdf->Cell(40,10,'Hello World!');
+    $pdf->WriteHTML($testhtml);
+
+    $separator = md5(time());
+
+    $headers = "MIME-Version: 1.0"; 
+    $headers .= "Content-Type: multipart/mixed; boundary=\"".$separator."\"";
+    $headers .= "Content-Transfer-Encoding: 7bit";
+
+    $pdf->Output($filename, "F"); // "F" is for saving the file on the server
+
+    $attachment = array($filename);
+
+    wp_mail( $email, $subject, $body, $headers, $attachment );
+
+    unlink($filename); // Deletion of the created file.
+    }
+?>
 
 <!-- THANK YOU MSG -->
 <?php if ( isset($_POST['time_estimate']) == 1 ) { ?>
