7871b3f5953f5c81f63f7d5d673ed2da99d3fa68
webozza <webozza@gmail.com>
2023-03-29 16:13:07 +0800

    version update

--- public/partials/evaluation-calc-public-display.php
+++ public/partials/evaluation-calc-public-display.php
@@ -18,8 +18,6 @@
 <!-- GLOBAL VARS -->
 <?php 
     $plugin_dir = '/wp-content/plugins/evaluation-calc';
-    $pdf = new FPDF();
-    var_dump($pdf);
 ?>
 
 <!-- THE FORM -->
@@ -900,26 +898,59 @@
             return 'Standard of Proof';
         };
 
-        //user posted variables
-        $name = $_POST['client_name'];
-        $email = $_POST['client_email'];
-        $message = $name . ' just made a calculation.';
+        $pdf = new FPDF('P', 'pt', array(500,233));
+        $pdf->AddFont('Georgiai','','georgiai.php');
+        $pdf->AddPage();
+        $pdf->Image('lib/fpdf/image.jpg',0,0,500);
+        $pdf->SetFont('georgiai','',16);
+        $pdf->Cell(40,10,'Hello World!');
 
-        //php mailer variables
-        $to = 'webozza@gmail.com';
-        $subject = $name . ' just made a calculation.';
-        $headers = 'From: '. $email . "\r\n" .
-            'Reply-To: ' . $email . "\r\n";
+        // email stuff (change data below)
+        $to = $_POST['client_email'];;
+        $from = "me@example.com"; 
+        $subject = "send email with pdf attachment"; 
+        $message = "<p>Please see the attachment.</p>";
 
-        // Here put your Validation and send mail
-        $sent = wp_mail($to, $subject, strip_tags($message), $headers);
-            
-        if($sent) {
-        //message sent!       
-        }
-        else  {
-        //message wasn't sent       
-        }
+        // a random hash will be necessary to send mixed content
+        $separator = md5(time());
+
+        // carriage return type (we use a PHP end of line constant)
+        $eol = PHP_EOL;
+
+        // attachment name
+        $filename = "test.pdf";
+
+        // encode data (puts attachment in proper format)
+        $pdfdoc = $pdf->Output("", "S");
+        $attachment = chunk_split(base64_encode($pdfdoc));
+
+        // main header
+        $headers  = "From: ".$from.$eol;
+        $headers .= "MIME-Version: 1.0".$eol; 
+        $headers .= "Content-Type: multipart/mixed; boundary=\"".$separator."\"";
+
+        // no more headers after this, we start the body! //
+
+        $body = "--".$separator.$eol;
+        $body .= "Content-Transfer-Encoding: 7bit".$eol.$eol;
+        $body .= "This is a MIME encoded message.".$eol;
+
+        // message
+        $body .= "--".$separator.$eol;
+        $body .= "Content-Type: text/html; charset=\"iso-8859-1\"".$eol;
+        $body .= "Content-Transfer-Encoding: 8bit".$eol.$eol;
+        $body .= $message.$eol;
+
+        // attachment
+        $body .= "--".$separator.$eol;
+        $body .= "Content-Type: application/octet-stream; name=\"".$filename."\"".$eol; 
+        $body .= "Content-Transfer-Encoding: base64".$eol;
+        $body .= "Content-Disposition: attachment".$eol.$eol;
+        $body .= $attachment.$eol;
+        $body .= "--".$separator."--";
+
+        // send message
+        wp_mail($to, $subject, $body, $headers);
     }
 ?>
 
