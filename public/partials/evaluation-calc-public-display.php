61bec1c26e3694c979104dfdfda3e32d1d650159
webozza <webozza@gmail.com>
2023-04-05 10:11:46 +0800

    testing pdf

--- public/partials/evaluation-calc-public-display.php
+++ public/partials/evaluation-calc-public-display.php
@@ -940,50 +940,6 @@
         function sender_name( $original_email_from ) {
             return 'Standard of Proof';
         };
-
-        /* Centering class
-        ---------------------------*/
-        class PDF extends FPDF {
-
-            const DPI = 96;
-            const MM_IN_INCH = 25.4;
-            const A4_HEIGHT = 297;
-            const A4_WIDTH = 210;
-            // tweak these values (in pixels)
-            const MAX_WIDTH = 800;
-            const MAX_HEIGHT = 500;
-        
-            function pixelsToMM($val) {
-                return $val * self::MM_IN_INCH / self::DPI;
-            }
-        
-            function resizeToFit($imgFilename) {
-                list($width, $height) = getimagesize($imgFilename);
-        
-                $widthScale = self::MAX_WIDTH / $width;
-                $heightScale = self::MAX_HEIGHT / $height;
-        
-                $scale = min($widthScale, $heightScale);
-        
-                return array(
-                    round($this->pixelsToMM($scale * $width)),
-                    round($this->pixelsToMM($scale * $height))
-                );
-            }
-        
-            function centreImage($img) {
-                list($width, $height) = $this->resizeToFit($img);
-        
-                // you will probably want to swap the width/height
-                // around depending on the page's orientation
-                $this->Image(
-                    $img, (self::A4_HEIGHT - $width) / 2,
-                    (self::A4_WIDTH - $height) / 2,
-                    $width,
-                    $height
-                );
-            }
-        }
             
         /* Email to User
         ---------------------------------------------------------------------------*/
@@ -1006,7 +962,7 @@
         $pdf->SetFont('helvetica','',16);
 
         // Add the Logo
-        $pdf->centreImage( $logo, 0, 0, 100 );
+        $pdf->Image( $logo, 100, 0, 100 );
         $pdf->Ln();
 
         // The Heading
