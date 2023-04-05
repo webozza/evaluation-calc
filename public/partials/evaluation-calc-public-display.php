77a59c62ee676cb9c6435884e4af0b42c01f233c
webozza <webozza@gmail.com>
2023-04-05 09:37:30 +0800

    testing pdf

--- public/partials/evaluation-calc-public-display.php
+++ public/partials/evaluation-calc-public-display.php
@@ -960,7 +960,7 @@
         $pdf->SetFont('helvetica','',16);
 
         // Add the Logo
-        $pdf->Image('/wp-content/plugins/evaluation-calc/public/img/logo.jpeg', 0, 0, 0, 0, 'JPEG');
+        $pdf->Image('https://wp.webozza.com/wp-content/plugins/evaluation-calc/public/img/logo.jpeg', 0, 0, 0, 0, 'JPEG');
         $pdf->Ln();
 
         // The Heading
