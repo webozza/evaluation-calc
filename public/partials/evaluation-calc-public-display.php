6462d147cd40557edbd08caf707067fd37a2e2a8
webozza <webozza@gmail.com>
2023-04-06 15:18:29 +0800

    version update

--- public/partials/evaluation-calc-public-display.php
+++ public/partials/evaluation-calc-public-display.php
@@ -972,7 +972,7 @@
         foreach($_POST['list__1'] as $list1) {
             $list1_pos = $list1_pos + 10;
             $pdf->SetFont('helvetica','',12);
-            $pdf->Cell(180, $list1_pos, '- ' . $list1, 0, 0, 'L');
+            $pdf->Cell(180, $list1_pos, '- ' . $list1, 0, 20, 'L');
             $pdf->Ln(0.1);
         }
         
