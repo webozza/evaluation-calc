aa0183c2366f300082bbb323ab30fb7698d5db32
webozza <webozza@gmail.com>
2023-04-06 15:58:55 +0800

    version update

--- public/partials/evaluation-calc-public-display.php
+++ public/partials/evaluation-calc-public-display.php
@@ -968,11 +968,11 @@
         $pdf->Ln(0.1);
 
         // List 1
-        $pdf->SetY(90);
+        $pdf->SetY(85);
         foreach($_POST['list__1'] as $list1) {
             $pdf->SetFont('helvetica','',12);
             //$pdf->Cell(180, $list1_pos, '- ' . $list1, 0, 0, 'L');
-            $pdf->MultiCell(176, 5, '- ' . $list1);
+            $pdf->MultiCell(150, 5, '- ' . $list1);
         }
         
         // $separator = md5(time());
