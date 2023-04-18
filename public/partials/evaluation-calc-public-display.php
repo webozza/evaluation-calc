e5beaeec9dffc2c590905d7a89b586850da7e4a2
webozza <webozza@gmail.com>
2023-04-18 12:34:23 +0800

    version update

--- public/partials/evaluation-calc-public-display.php
+++ public/partials/evaluation-calc-public-display.php
@@ -975,7 +975,7 @@
         $pdf->AddPage();    
 
         // Add the Logo
-        $pdf->Image( $logo, 28, 5, 100 );
+        $pdf->Image( $logo, 55, 5, 50 );
         $pdf->Ln(0.1);
 
         // Add the Meter
