fbe2941ce7c21a9fae9ecf6a8d7c0e385d7d1f3a
webozza <webozza@gmail.com>
2023-05-22 22:52:16 +0800

    version update

--- evaluation-calc.php
+++ evaluation-calc.php
@@ -34,7 +34,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'EVALUATION_CALC_VERSION', '7.7.39' );
+define( 'EVALUATION_CALC_VERSION', '7.7.40' );
 
 /**
  * The code that runs during plugin activation.
